47f01987b5dbd55d71c9db8915bf7b7f420c8f11
sleeprince <wowkwangsu@gmail.com>
2024-07-26 10:36:46 +0900

    disabled

--- src/test/java/com/app/SpringGitTestApplicationTests.java
+++ src/test/java/com/app/SpringGitTestApplicationTests.java
@@ -1,13 +1,14 @@
 package com.app;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringGitTestApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test @Disabled
+	void contextLoads() {
+	}
 
 }
